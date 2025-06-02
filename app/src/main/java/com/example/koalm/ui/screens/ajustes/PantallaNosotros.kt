a9cfea83248c8945603e7aa437fdb75fc40d8c93
Ricardo Mora <mora.santillan.ricardo@gmail.com>
2025-06-01 22:08:36 -0600

    Corrección de ajustes

--- app/src/main/java/com/example/koalm/ui/screens/ajustes/PantallaNosotros.kt
+++ app/src/main/java/com/example/koalm/ui/screens/ajustes/PantallaNosotros.kt
@@ -12,12 +12,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
