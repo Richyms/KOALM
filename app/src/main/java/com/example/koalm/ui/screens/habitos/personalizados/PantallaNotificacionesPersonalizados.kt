63999311089f124791be081568c30e9cc7c71c1a
Rig241 <hocanac1900@alumno.ipn.mx>
2025-06-14 21:53:56 -0600

    Cambios en las tarjetas de notificaciones

--- app/src/main/java/com/example/koalm/ui/screens/habitos/personalizados/PantallaNotificacionesPersonalizados.kt
+++ app/src/main/java/com/example/koalm/ui/screens/habitos/personalizados/PantallaNotificacionesPersonalizados.kt
@@ -33,6 +33,7 @@ import com.example.koalm.data.HabitosRepository.obtenerHabitosPersonalizados
 import com.example.koalm.services.notifications.AlimentationNotificationService
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -52,6 +53,7 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.example.koalm.ui.components.obtenerIconoPorNombre
@@ -66,6 +68,7 @@ import com.example.koalm.ui.components.ExitoDialogoGuardadoAnimado
 
 import com.google.firebase.auth.FirebaseAuth
 import com.example.koalm.services.notifications.NotificationBase
+import com.example.koalm.ui.screens.HabitoCardPersonalizado
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -128,13 +131,20 @@ fun PantallaNotificacionesPersonalizados(navController: NavHostController){
     ) { innerPadding  ->
         Column(
             modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(24.dp),
+                .padding(innerPadding)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(24.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center)
         {
+            Text(
+                text = "Hábitos por realizar",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
             val habitosActivos = habitos.value.filter { it.estaActivo }
             if (isLoading.value) {
                 CircularProgressIndicator(modifier = Modifier.size(40.dp))
@@ -176,7 +186,7 @@ fun PantallaNotificacionesPersonalizados(navController: NavHostController){
                     )
                 } else {
                     habitosActivos.forEach { habito ->
-                        HabitoCardExpandible(
+                        HabitoCardPerso(
                             habito = habito,
                             navController = navController,
                             onEliminarHabito = {
@@ -190,5 +200,90 @@ fun PantallaNotificacionesPersonalizados(navController: NavHostController){
                 }
             }
         }
+    }
+}
+
+@Composable
+fun HabitoCardPerso(
+    habito: HabitoPersonalizado,
+    navController: NavHostController,
+    onEliminarHabito: () -> Unit = {},
+    colorPersonalizado: Color? = null
+) {
+    var mostrarDialogoConfirmacion by remember { mutableStateOf(false) }
+    var mostrarDialogoExito by remember { mutableStateOf(false) }
+
+    if (mostrarDialogoExito) {
+        ExitoDialogoGuardadoAnimado(
+            mensaje = "¡Hábito eliminado con éxito!",
+            onDismiss = {
+                mostrarDialogoExito = false
+                onEliminarHabito()
+            }
+        )
+    }
+
+    val icono = obtenerIconoPorNombre(habito.iconoEtiqueta)
+    val colorTarjeta = colorPersonalizado ?: parseColorFromFirebase(habito.colorEtiqueta)
+    val colorIcono = colorPersonalizado ?: parseColorFromFirebase(habito.colorEtiqueta, darken = true)
+
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(containerColor = colorTarjeta),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Box(
+                    modifier = Modifier
+                        .size(24.dp)
+                        .shadow(4.dp, shape = RoundedCornerShape(8.dp), clip = false)
+                        .background(Color.White, shape = RoundedCornerShape(8.dp)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = icono,
+                        contentDescription = "Icono del Hábito",
+                        tint = colorIcono,
+                        modifier = Modifier.size(18.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        text = habito.nombre,
+                        style = MaterialTheme.typography.bodyMedium.copy(fontSize = 24.sp ,fontWeight = FontWeight.Bold),
+                    )
+                    if (habito.descripcion.isNotBlank()) {
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = habito.descripcion,
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+        }
+    }
+
+    if (mostrarDialogoConfirmacion) {
+        ConfirmacionDialogoEliminarAnimado(
+            habitoNombre = habito.nombre,
+            onCancelar = { mostrarDialogoConfirmacion = false },
+            onConfirmar = {
+                mostrarDialogoConfirmacion = false
+                eliminarHabitoPersonalizado(
+                    nombreHabito = habito.nombre,
+                    usuarioEmail = FirebaseAuth.getInstance().currentUser?.email,
+                    onSuccess = { mostrarDialogoExito = true }
+                )
+            }
+        )
     }
 }
