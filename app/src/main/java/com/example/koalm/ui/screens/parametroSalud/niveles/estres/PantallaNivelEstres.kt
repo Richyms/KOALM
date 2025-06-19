5e454d993b7df24ab8e1dde4b67108dca511cf0b
Rig241 <hocanac1900@alumno.ipn.mx>
2025-06-18 20:31:16 -0600

    Ajuste para reflejar resultados de test de ansiedad

--- app/src/main/java/com/example/koalm/ui/screens/parametroSalud/niveles/estres/PantallaNivelEstres.kt
+++ app/src/main/java/com/example/koalm/ui/screens/parametroSalud/niveles/estres/PantallaNivelEstres.kt
@@ -9,22 +9,36 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.SentimentNeutral
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.koalm.ui.components.*
 import com.example.koalm.ui.theme.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PantallaEstres(
-    navController: NavHostController,
-    datos: DatosEstres = datosMockEstres
+    navController: NavHostController
 ) {
+    val context = LocalContext.current
+    val correo = FirebaseAuth.getInstance().currentUser?.email
+    val ansiedad = remember(correo) {
+        Firebase.firestore.collection("resultadosAnsiedad")
+            .document(correo ?: "")
+            .collection("historial")
+            .document("Resultado 1")
+    }
+    val ResAnsiedad by ansiedad.snapshotsAsState { it?.getString("nivel")?.toString()}
     Scaffold(
         topBar = {
             TopAppBar(
@@ -58,8 +72,8 @@ fun PantallaEstres(
                 )
                 Spacer(modifier = Modifier.width(8.dp))
                 Text(
-                    text = datos.nivel,
-                    fontSize = 36.sp,
+                    text = "${ResAnsiedad}",
+                    fontSize = 24.sp,
                     fontWeight = FontWeight.Bold
                 )
             }
@@ -78,8 +92,14 @@ fun PantallaEstres(
                             .height(180.dp)
                     ) {
                         GraficaEstres(
-                            valores = datos.valores,
-                            colores = datos.colores
+                            valores = listOf(0.6f, 0.7f, 1f, 0.8f, 0.4f, 0.6f, 0.5f, 0.3f, 0.2f, 0.4f, 0.3f, 0.5f, 0.6f, 0.4f, 0.8f, 0.7f, 0.6f, 0.5f, 0.7f, 0.9f, 1f, 0.9f, 0.6f, 0.3f),
+                            colores = listOf(0.6f, 0.7f, 1f, 0.8f, 0.4f, 0.6f, 0.5f, 0.3f, 0.2f, 0.4f, 0.3f, 0.5f, 0.6f, 0.4f, 0.8f, 0.7f, 0.6f, 0.5f, 0.7f, 0.9f, 1f, 0.9f, 0.6f, 0.3f).map {
+                                when {
+                                    it > 0.8f -> MarronKoala
+                                    it > 0.5f -> GrisMedio
+                                    else -> VerdePrincipal
+                                }
+                            }
                         )
                     }
 
@@ -112,14 +132,14 @@ fun PantallaEstres(
                                         .background(VerdePrincipal, CircleShape)
                                 )
                                 Spacer(modifier = Modifier.width(8.dp))
-                                Text(datos.promedio)
+                                Text("Medio")
                             }
                         }
 
                         Column {
                             Text("Mayor estrés", fontWeight = FontWeight.SemiBold, fontSize = 13.sp)
                             Spacer(modifier = Modifier.height(4.dp))
-                            Text("${datos.mayorInicio} - ${datos.mayorFin}")
+                            Text("${ResAnsiedad}")
                         }
                     }
                 }
